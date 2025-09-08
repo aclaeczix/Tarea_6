1714ee3ab8015b4a152cc2b045d7b03cd736f152
aclaeczix <alyaalme12@yahoo.com>
2025-09-08 00:15:05 -0600

    Update WordCounter.ts

--- WordCounter.ts
+++ WordCounter.ts
@@ -13,27 +13,30 @@ class WordCounter {
     let word = "";
     const map = new Map<string, number>();
 
+
     for (let i = 0; i < this.text.length; i++) {
       const char = this.text[i];
       
       if (char === " ") {
-        if (word !== "") {
-          if (map.has(word)) {
-            map.set(word, map.get(word)! + 1);
-          } else {
-            map.set(word, 1);
-          }
-          word = "";
+        if (map.has(word)) {
+          const val = map.get(word);
+
+          map.set(word, val!+1);
+        } else {
+          map.set(word, 1);
         }
+
+        word = ""
       } else {
-        word += char;
+        word += char;  // word = word + char
       }
     }
 
-    
+    // 👇 único agregado para guardar la última palabra
     if (word !== "") {
       if (map.has(word)) {
-        map.set(word, map.get(word)! + 1);
+        const val = map.get(word);
+        map.set(word, val! + 1);
       } else {
         map.set(word, 1);
       }
@@ -43,12 +46,13 @@ class WordCounter {
   }
 }
 
+
 const text = "Una vida sin pan o una vida feliz";
 const wordCounter = new WordCounter(text);
 console.log(wordCounter.getText());
 
 const result = wordCounter.counter();
 
-result.forEach((count, word) => {
-  console.log(word, "-", count);
-});
+result.forEach((k, v) => {
+  console.log(k, "-", v);
+})
